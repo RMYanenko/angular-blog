e27517671b112c9742a21b9c47a5803df9520156
RMYanenko <yanenko.r.m@ukr.ner>
2020-04-01 12:46:00 +0300

    Create guard for dashboard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
@@ -14,13 +14,21 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
   submitted = false;
+  message: string;
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private  route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Please log in';
+      }
+    });
+
     this.form = new FormGroup({ // ініціалізація форми, валідація має два контроли пароль і мейл
       email: new FormControl(null, [
         Validators.required,
